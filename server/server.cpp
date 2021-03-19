872f119ce2b09abf22d23637f4fa1f62a43b3761
mcopik <mcopik@gmail.com>
2021-03-20 00:56:37 +0100

    Add new executables for server

--- server/server.cpp
+++ server/server.cpp
@@ -1,157 +1,284 @@
 
-#include <chrono>
-#include <stdexcept>
-#include <thread>
-#include <climits>
+#include <csignal>
+#include <signal.h>
 #include <sys/time.h>
 
-#include <signal.h>
-#include <cxxopts.hpp>
 #include <spdlog/spdlog.h>
 
-#include <rdmalib/rdmalib.hpp>
-#include <rdmalib/server.hpp>
-#include "rdmalib/connection.hpp"
+#include "rdmalib/buffer.hpp"
+#include "rdmalib/rdmalib.hpp"
+#include "rdmalib/server.hpp"
 #include "server.hpp"
 
+namespace server {
 
-int main(int argc, char ** argv)
-{
-  server::SignalHandler sighandler;
-  auto opts = server::opts(argc, argv);
-  if(opts.verbose)
-    spdlog::set_level(spdlog::level::debug);
-  else
-    spdlog::set_level(spdlog::level::info);
-  spdlog::set_level(spdlog::level::debug);
-  spdlog::set_pattern("[%H:%M:%S:%f] [T %t] [%l] %v ");
-  spdlog::info("Executing serverless-rdma server!");
+  bool SignalHandler::closing = false;
 
-  // Start RDMA connection
-  server::Server server(
-      opts.address,
-      opts.port,
-      opts.cheap_executors,
-      opts.fast_executors,
-      opts.msg_size,
-      opts.recv_buffer_size,
-      opts.pin_threads,
-      opts.max_inline_data,
-      opts.server_file
-  );
+  SignalHandler::SignalHandler()
+  {
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = &SignalHandler::handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    //FIXME: disable signals to avoid potential interrupts
+    //sigaction(SIGINT, &sigIntHandler, nullptr);
+  }
 
-  // Wait for a client
-  // FIXME: handle more clients
-  auto conn = server.poll_communication();
-  if(!conn)
-    return -1;
-  // TODO: Display client's address
-  spdlog::info("Connected a client!");
+  void SignalHandler::handler(int)
+  {
+    SignalHandler::closing = true;
+  }
 
-  int sum = 0, repetitions = 0;
-  if(opts.polling_manager == server::Options::PollingMgr::SERVER)
-    std::tie(sum, repetitions) = server.poll_server(opts.repetitions, opts.warmup_iters);
-  else
-    std::tie(sum, repetitions) = server.poll_threads(opts.repetitions, opts.warmup_iters);
+  Server::Server(std::string addr, int port, int cheap_executors, int fast_executors,
+      int msg_size, int rcv_buf, bool pin_threads, int max_inline_data, std::string server_file):
+    _state(addr, port, rcv_buf, true, max_inline_data),
+    _status(addr, port),
+    _fast_exec(fast_executors, msg_size, pin_threads, *this),
+    _conn(nullptr),
+    _wc_buffer(rcv_buf),
+    _inline_data(msg_size <= max_inline_data)
+  {
+    //listen();
 
-  spdlog::info("Server is closing down, avg response time {} usec", ((float)sum)/repetitions);
-  std::this_thread::sleep_for(std::chrono::seconds(1)); 
+    // TODO: optimize, single buffer + offsets
+    // TODO: share between clients?
+    // FIXME: valid only for the "cheap" exeuction
+    //for(int i = 0; i < QUEUE_SIZE; ++i) {
+    //  _queue[i] = std::move(rdmalib::Buffer<char>(QUEUE_MSG_SIZE));
+    //  _queue[i].register_memory(_state.pd(), IBV_ACCESS_LOCAL_WRITE);
+    //}
+    // Initialize threads as currently unbusy
+    //memset(_threads_allocation.data(), 0, _threads_allocation.data_size());
+    //_threads_allocation.register_memory(_state.pd(), IBV_ACCESS_REMOTE_ATOMIC | IBV_ACCESS_REMOTE_WRITE |IBV_ACCESS_LOCAL_WRITE);
+    //_status.set_thread_allocator(_threads_allocation);
+    std::ofstream out(server_file);
+    status().serialize(out);
+  }
 
-  //int sum = 0;
-  //int repetitions = 0;
-  //timeval start, end;
-  //constexpr int cores_mask = 0x3F;
-  //while(!server::SignalHandler::closing) {
-  //  // if we block, we never handle the interruption
-  //  std::optional<ibv_wc> wc = conn->poll_wc(rdmalib::QueueType::RECV, false);
-  //  bool correct_allocation = true;
-  //  if(wc) {
+  const rdmalib::server::ServerStatus & Server::status() const
+  {
+    return _status;
+  }
 
-  //    gettimeofday(&start, nullptr);
-  //    if(wc->status) {
-  //      spdlog::error("Failed work completion! Reason: {}", ibv_wc_status_str(wc->status));
-  //      continue;
-  //    }
-  //    int info = ntohl(wc->imm_data);
-  //    int func_id = info >> 6;
-  //    int core = info & cores_mask;
-  //    SPDLOG_DEBUG("Execute func {} at core {}", func_id, core);
-  //    //uint32_t cur_invoc = server._exec.get_invocation_id();
-  //    uint32_t cur_invoc = 0;
-  //    //server::InvocationStatus & invoc = server._exec.invocation_status(cur_invoc);
-  //    //invoc.connection = &*conn;
-  //    server._exec.enable(core,
-  //      {
-  //        server._db.functions[func_id],
-  //        &server._rcv[core],
-  //        &server._send[core],
-  //        cur_invoc
+  void Server::listen()
+  {
+    _state.allocate();
+  }
+
+  void Server::reload_queue(rdmalib::Connection & conn, int32_t idx)
+  {
+    SPDLOG_DEBUG("Post receive idx: {}", idx);
+    // FIXME: "cheap"
+    //conn.post_recv(_queue[idx], idx);
+    conn.post_recv({});
+  }
+
+  rdmalib::Connection* Server::poll_communication()
+  {
+    // TODO: timeout + number of retries option
+    this->_conn = _state.poll_events(
+      [this](rdmalib::Connection & conn) {
+        this->_wc_buffer.connect(&conn);
+      }
+    );
+    if(this->_conn)
+      this->_conn->inlining(_inline_data);
+    return this->_conn;
+  }
+
+  std::tuple<int, int> Server::poll_server(int max_repetitions, int warmup_iters)
+  {
+    _fast_exec.allocate_threads(false);
+
+    int repetitions = 0;
+    int total_iters = max_repetitions + warmup_iters;
+    constexpr int cores_mask = 0x3F;
+    timeval start;
+    while(!server::SignalHandler::closing && repetitions < total_iters) {
+
+      // if we block, we never handle the interruption
+      auto wcs = _wc_buffer.poll();
+      if(std::get<1>(wcs)) {
+        // FIXME: one var per thread
+        gettimeofday(&start, nullptr);
+        for(int i = 0; i < std::get<1>(wcs); ++i) {
+
+          ibv_wc* wc = &std::get<0>(wcs)[i];
+          if(wc->status) {
+            spdlog::error("Failed work completion! Reason: {}", ibv_wc_status_str(wc->status));
+            continue;
+          }
+          int info = ntohl(wc->imm_data);
+          int func_id = info >> 6;
+          int core = info & cores_mask;
+          // Startpoint of iteration for a given thread
+          _fast_exec._start_timestamps[core] = start;
+          SPDLOG_DEBUG("Execute func {} at core {}", func_id, core);
+          //uint32_t cur_invoc = server._exec.get_invocation_id();
+          uint32_t cur_invoc = 0;
+
+          // FIXME: producer-consumer interface
+          SPDLOG_DEBUG("Wake-up fast thread {}", core);
+          _fast_exec.enable(core,
+            {
+              _db.functions[func_id],
+              cur_invoc,
+              this->_conn
+            }
+          );
+
+          // clean send queue
+          this->_conn->poll_wc(rdmalib::QueueType::SEND, false);
+          _wc_buffer.refill();
+          ++repetitions;
+        }
+      }
+    }
+    _fast_exec.close();
+
+    return std::make_tuple(_fast_exec._time_sum.load(), repetitions);
+  }
+
+  std::tuple<int, int> Server::poll_threads(int max_repetitions, int warmup_iters)
+  {
+    _fast_exec._conn = _conn;
+    _fast_exec._wc_buffer = &_wc_buffer;
+    // FIXME: parallel accumulation of reps across threads?
+    // +1 to handle the warm-up call
+    _fast_exec._max_repetitions = max_repetitions;
+    _fast_exec._warmup_iters = warmup_iters;
+    _fast_exec.allocate_threads(true);
+
+    // FIXME: more threads
+    _fast_exec._threads[0].join();
+
+    spdlog::info("{} {}", _fast_exec._time_sum.load(), _fast_exec._repetitions.load());
+
+    return std::make_tuple(_fast_exec._time_sum.load(), _fast_exec._repetitions.load());
+  }
+
+  // FIXME: shared receive queue
+  //void Server::poll_srq()
+  //{
+  //  while(!server::SignalHandler::closing) {
+
+  //    // if we block, we never handle the interruption
+  //    std::optional<ibv_wc> wc = conn->poll_wc(rdmalib::QueueType::RECV, false);
+  //    bool correct_allocation = true;
+  //    if(wc) {
+
+  //      gettimeofday(&start, nullptr);
+  //      if(wc->status) {
+  //        spdlog::error("Failed work completion! Reason: {}", ibv_wc_status_str(wc->status));
+  //        continue;
   //      }
-  //    );
-  //    //server._exec.wakeup();
-  //    server._exec.work(core);
-  //    // clean queue
-  //    conn->poll_wc(rdmalib::QueueType::SEND, false);
-  //    gettimeofday(&end, nullptr);
-  //    int usec = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
-  //    repetitions += 1;
-  //    requests--;
-  //    if(requests < 1) {
-  //      conn->post_recv({}, -1, server._rcv_buf_size);
-  //      requests = server._rcv_buf_size;
-  //    }
-  //    sum += usec;
-
-
-  //    // Reenable: code for cheap invocation
-  //    // Insert new rcv reequest
-  //    //server.reload_queue(*conn, req_id);
-  //    //rdmalib::functions::Submission * ptr = reinterpret_cast<rdmalib::functions::Submission*>(
-  //    //    server._queue[wc->wr_id].data()
-  //    //);
-
-  //    //// Verify that core allocation makes sense
-  //    //if(ptr->core_begin >= ptr->core_end || ptr->core_begin < 0 || ptr->core_end > numcores) {
-  //    //  spdlog::error("Incorrect core allocation from {} to {}!", ptr->core_begin, ptr->core_end);
-  //    //  correct_allocation = false;
-  //    //}
-  //    //for(int i = ptr->core_begin; i < ptr->core_end; ++i)
-  //    //  if(!(*server._threads_allocation.data() & (1 << i))) {
-  //    //    spdlog::error(
-  //    //        "Requested allocating core {}, but current allocator is {}!",
-  //    //        i,
-  //    //        *server._threads_allocation.data()
-  //    //    );
-  //    //    correct_allocation = false;
-  //    //  }
-
-
-  //    //if(correct_allocation) {
-  //    //  uint32_t cur_invoc = server._exec.get_invocation_id();
-  //    //  server::InvocationStatus & invoc = server._exec.invocation_status(cur_invoc);
-  //    //  invoc.active_threads.store(ptr->core_end - ptr->core_begin);
-  //    //  invoc.connection = &*conn;
-  //    //  spdlog::info(
-  //    //      "Accepted invocation of function of id {}, internal invocation idx {}",
-  //    //      ptr->ID,
-  //    //      cur_invoc
-  //    //  );
-
-  //    //  for(int i = ptr->core_begin; i != ptr->core_end; ++i)
-  //    //    server._exec.enable(i,
-  //    //      {
-  //    //        server._db.functions[ptr->ID],
-  //    //        &server._rcv[i],
-  //    //        &server._send[i],
-  //    //        cur_invoc
-  //    //      }
-  //    //    );
-  //    //  server._exec.wakeup();
-  //    //} else {
-  //    //  // TODO: send a reply that something went wrong
-  //    //}
+  //      int info = ntohl(wc->imm_data);
+  //      int func_id = info >> 6;
+  //      int core = info & cores_mask;
+  //      SPDLOG_DEBUG("Execute func {} at core {}", func_id, core);
+  //      //uint32_t cur_invoc = server._exec.get_invocation_id();
+  //      uint32_t cur_invoc = 0;
+  //      server::InvocationStatus & invoc = server._exec.invocation_status(cur_invoc);
+  //      invoc.connection = &*conn;
+  //      server._exec.enable(core,
+  //        {
+  //          server._db.functions[func_id],
+  //          &server._rcv[core],
+  //          &server._send[core],
+  //          cur_invoc
+  //        }
+  //      );
+  //      //server._exec.wakeup();
+  //      server._exec.work(core);
+  //      // clean queue
+  //      conn->poll_wc(rdmalib::QueueType::SEND, false);
+  //      gettimeofday(&end, nullptr);
+  //      int usec = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+  //      repetitions += 1;
+  //      requests--;
+  //      if(requests < 1) {
+  //        conn->post_recv({}, -1, server._rcv_buf_size);
+  //        requests = server._rcv_buf_size;
+  //      }
+  //      sum += usec;
   //  }
   //}
 
-  return 0;
+
+  //uint32_t Executors::get_invocation_id()
+  //{
+  //  if(_last_invocation == _numcores) {
+  //    _last_invocation = 0;
+  //    // find next empty
+  //    while(_invocations_status[_last_invocation++].connection);
+  //    _last_invocation--;
+  //  }
+  //  return _last_invocation;
+  //}
+
+  //InvocationStatus & Executors::invocation_status(int idx)
+  //{
+  //  return this->_invocations_status[idx];
+  //}
+
+  //void Executors::thread_func(int id)
+  //{
+  //  std::unique_lock<std::mutex> lk(m);
+  //  rdmalib::functions::FuncType ptr = nullptr;
+  //  SPDLOG_DEBUG("Thread {} created!", id);
+  //  while(!_closing) {
+
+  //    SPDLOG_DEBUG("Thread {} goes to sleep! {}", id, _closing);
+  //    if(!lk.owns_lock())
+  //      lk.lock();
+  //    _cv.wait(lk, [this, id](){ return _threads_status[id].func || _closing; });
+  //    lk.unlock();
+  //    SPDLOG_DEBUG("Thread {} wakes up! {}", id, _closing);
+
+  //    if(_closing) {
+  //      SPDLOG_DEBUG("Thread {} exits!", id);
+  //      return;
+  //    }
+
+  //    ptr = _threads_status[id].func;
+  //    uint32_t invoc_id = _threads_status[id].invoc_id;
+  //    //rdmalib::Connection* conn = std::get<3>(_status[id]);
+
+  //    SPDLOG_DEBUG("Thread {} begins work! Executing function", id);
+  //    // Data to ignore header passed in the buffer
+  //    (*ptr)(_threads_status[id].in->data(), _threads_status[id].out->ptr());
+  //    SPDLOG_DEBUG("Thread {} finished work!", id);
+
+  //    char* data = static_cast<char*>(_threads_status[id].in->ptr());
+  //    uint64_t r_addr = *reinterpret_cast<uint64_t*>(data);
+  //    uint32_t r_key = *reinterpret_cast<uint32_t*>(data + 8);
+  //    SPDLOG_DEBUG("Thread {} finished work! Write to remote addr {} rkey {}", id, r_addr, r_key);
+  //    // decrease number of active instances
+  //    if(--_invocations_status[invoc_id].active_threads) {
+  //      // write result
+  //      _invocations_status[invoc_id].connection->post_write(
+  //        *_threads_status[id].out,
+  //        {r_addr, r_key}
+  //      );
+  //    } else {
+  //      uint32_t func_id = *reinterpret_cast<uint32_t*>(data + 12);
+  //      // write result and signal
+  //      _invocations_status[invoc_id].connection->post_write(
+  //        *_threads_status[id].out,
+  //        {r_addr, r_key},
+  //        func_id
+  //      );
+  //      // TODO Clean invocation status
+  //    }
+
+  //    // clean status of thread
+  //    // TODO: atomic status
+  //    _threads_status[id] = {nullptr, nullptr, nullptr, 0};
+  //    this->disable(id);
+  //    ptr = nullptr;
+
+  //    SPDLOG_DEBUG("Thread {} loops again!", id);
+  //  } 
+  //  SPDLOG_DEBUG("Thread {} exits!", id);
+  //}
 }
