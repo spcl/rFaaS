df6423c601226a7caa61ad121263755fc71e85fb
mcopik <mcopik@gmail.com>
2021-03-26 17:10:54 +0100

    Add cold benchmark

---
+++ tests/cold_benchmark.hpp
@@ -0,0 +1,39 @@
+
+#ifndef __TESTS__COLD_BENCHMARKER_HPP__
+#define __TESTS__COLD_BENCHMARKER_HPP__
+
+#include <string>
+
+namespace cold_benchmarker {
+
+  struct Options {
+
+    enum class PollingMgr {
+      SERVER=0,
+      SERVER_NOTIFY,
+      THREAD
+    };
+
+    enum class PollingType {
+      WC=0,
+      DRAM
+    };
+
+    std::string server_file;
+    int repetitions;
+    int warmup_iters;
+    std::string fname;
+    int input_size;
+    std::string out_file;
+    bool pin_threads;
+    int recv_buf_size;
+    int max_inline_data;
+    bool verbose;
+  };
+
+  Options opts(int argc, char ** argv);
+
+}
+
+#endif
+
