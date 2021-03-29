239a59f3fb90336231e3bd6b250fd252ab7a5590
mcopik <mcopik@gmail.com>
2021-03-30 01:47:39 +0200

    Add functions loaded as shared library

---
+++ server/functions.hpp
@@ -0,0 +1,31 @@
+
+#ifndef __SERVER_FUNCTIONS_HPP__
+#define __SERVER_FUNCTIONS_HPP__
+
+#include <vector>
+#include <string>
+
+namespace server {
+
+  void extract_symbols(void* handle, std::vector<std::string> & names);
+
+  struct Functions
+  {
+    int _fd;
+    void* _memory_handle;
+    size_t _size;
+    void* _library_handle;
+    // FIXME: small vector?
+    std::vector<std::string> _names;
+    std::vector<void*> _functions;
+
+    typedef void (*FuncType)(void*, uint32_t, void*);
+
+    Functions(size_t size);
+    ~Functions();
+    FuncType function(int idx);
+  };
+
+}
+
+#endif
