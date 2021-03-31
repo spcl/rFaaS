16c0f726356e355cb5d3ee18554fe582c7baa207
mcopik <mcopik@gmail.com>
2021-03-31 16:20:07 +0200

    Slightly more verbose init

--- server/executor.cpp
+++ server/executor.cpp
@@ -25,7 +25,7 @@ int main(int argc, char ** argv)
     spdlog::set_level(spdlog::level::info);
   spdlog::set_level(spdlog::level::debug);
   spdlog::set_pattern("[%H:%M:%S:%f] [T %t] [%l] %v ");
-  spdlog::info("Executing serverless-rdma server!");
+  spdlog::info("Executing serverless-rdma server! Waiting for client at {}:{}", opts.address, opts.port);
 
   server::FastExecutors executor(
     opts.address, opts.port,
