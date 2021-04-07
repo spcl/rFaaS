ae4a122fbfa7f93e4c808876842c7baaffe949e7
Marcin Copik <mcopik@gmail.com>
2021-04-07 03:03:18 +0200

    Update run script

--- build/run_cold_experiment.sh
+++ build/run_cold_experiment.sh
@@ -2,19 +2,24 @@
 
 trap 'echo -ne "Stop tests...\n"  && killAllProcesses && exit 1' INT
 
-define(){ IFS='\n' read -r -d '' ${1} || true; }
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+RHOME=$(pwd)
+echo $RHOME
 
-REMOTE_IP=192.168.1.10
-PORT2=9999
+REMOTE_IP=148.187.105.13
+PORT2=10001
+
+LOCAL_IP=148.187.105.11
+PORT1=10002
 
 killAllProcesses(){
+	  ssh ${USER}@${REMOTE_IP} "killall -s 9 executor_manager"
     if [ ! -z ${rpid+x} ]; then
 	    echo "Killing remote processes with PID$rpid"
-	    ssh ${USER}@${REMOTE_IP} "kill -9 $rpid"
+	    #ssh ${USER}@${REMOTE_IP} "kill -9 $rpid"
     fi
 }
 
+killAllProcesses
 
 size=1024
 RX_DEPTH=512
@@ -22,15 +27,16 @@ REPETITIONS=1000
 WARMUP=100
 OPTIONS="--polling-mgr thread --polling-type wc --pin-threads true --max-inline-data 64"
 CLIENT_OPTIONS="--max-inline-data 64"
-cmd="taskset 0x3 ${SCRIPTPATH}/bin/executor_manager -a ${REMOTE_IP} -p $PORT2 -v -f ${SCRIPTPATH}/server.json"
-sshcmd=( "ssh" "${USER}@${REMOTE_IP}" "nohup" "${cmd}" "> ${SCRIPTPATH}/serverless_server_$size 2>&1" "&" "echo \$!" )
-echo "${sshcmd[@]}"
-rpid=$("${sshcmd[@]}")   
+cmd="PATH=$RHOME/bin/:$PATH ${RHOME}/bin/executor_manager -a ${REMOTE_IP} -p $PORT2 -v -f -r 1 -x 32 --warmup-iters 0 --max-inline-data 128 ${RHOME}/server.json"
+#sshcmd=( "ssh" "${USER}@${REMOTE_IP}" "nohup" "${cmd}" "> ${RHOME}/serverless_server_$size 2>&1" "&" "echo \$!" )
+#echo "${sshcmd[@]}"
+#rpid=$("${sshcmd[@]}")   
+ssh -f -M ${USER}@${REMOTE_IP} "${cmd} > ${RHOME}/serverless_server_$size 2>&1"
 
 echo "Sshed the server with PID${rpid}"
-sleep 3
+sleep 1
 echo "Start client locally"
-${SCRIPTPATH}/tests/cold_benchmarker -f ${SCRIPTPATH}/server.json -r 1000 --out-file out.csv -s 1024 -n empty --recv-buf-size 32 -v  #> ${SCRIPTPATH}/serverless_client_$size
+${RHOME}/tests/cold_benchmarker -a ${LOCAL_IP} -p ${PORT1} -f ${RHOME}/servers.json -r 1000 --out-file out.csv -s 32 --fname empty --recv-buf-size 32 -v  --fname empty --flib ${RHOME}/examples/libfunctions.so --max-inline-data 128 -c 1 --hot-timeout -1
 
 echo "test is done"
 
