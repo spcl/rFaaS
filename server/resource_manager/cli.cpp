3839e7b0ad3dd3b78fb5ffa2683abb21d2913284
Marcin Copik <mcopik@gmail.com>
2021-08-06 21:56:58 +0200

    [resource-mgr] Fix a typo

--- server/resource_manager/cli.cpp
+++ server/resource_manager/cli.cpp
@@ -35,7 +35,7 @@ int main(int argc, char ** argv)
     spdlog::set_level(spdlog::level::info);
   spdlog::set_level(spdlog::level::debug);
   spdlog::set_pattern("[%H:%M:%S:%f] [P %P] [T %t] [%l] %v ");
-  spdlog::info("Executing rFaaS executor manager!");
+  spdlog::info("Executing rFaaS resource manager!");
 
   // Catch SIGINT
   struct sigaction sigIntHandler;
