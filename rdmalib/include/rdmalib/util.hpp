8cca2dda1bc4f9099e377e1400891566cc8c8374
Marcin Copik <mcopik@gmail.com>
2022-09-27 23:56:34 +0200

    [rdmalib] Add string formatting function

--- rdmalib/include/rdmalib/util.hpp
+++ rdmalib/include/rdmalib/util.hpp
@@ -78,6 +78,19 @@ namespace rdmalib { namespace impl {
     assert(ptr);
   }
 
+	// Code borrowed from StackOverflow https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
+	template<typename ... Args>
+	std::string string_format( const std::string& format, Args ... args )
+	{
+			int size_s = std::snprintf( nullptr, 0, format.c_str(), args ... ) + 1;
+			if(size_s <= 0)
+				return "";
+			auto size = static_cast<size_t>(size_s);
+			std::unique_ptr<char[]> buf(new char[size]);
+			std::snprintf(buf.get(), size, format.c_str(), args...);
+			return std::string(buf.get(), buf.get() + size - 1);
+	}
+
 }}
 
 #endif
