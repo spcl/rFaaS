453a1cdee202522a87e4d91d2ddb03f4f45dec47
Marcin Copik <mcopik@gmail.com>
2022-09-28 03:05:03 +0200

    [util] Support strings in formatting

--- rdmalib/include/rdmalib/util.hpp
+++ rdmalib/include/rdmalib/util.hpp
@@ -78,16 +78,22 @@ namespace rdmalib { namespace impl {
     assert(ptr);
   }
 
+  template<typename StrType>
+  const char* to_cstr(const StrType & str)
+  {
+    return str.c_str();
+  }
+
 	// Code borrowed from StackOverflow https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
 	template<typename ... Args>
-	std::string string_format( const std::string& format, Args ... args )
+	std::string string_format( const std::string& format, Args... args )
 	{
-			int size_s = std::snprintf( nullptr, 0, format.c_str(), args ... ) + 1;
+			int size_s = std::snprintf( nullptr, 0, format.c_str(), to_cstr(args)... ) + 1;
 			if(size_s <= 0)
 				return "";
 			auto size = static_cast<size_t>(size_s);
 			std::unique_ptr<char[]> buf(new char[size]);
-			std::snprintf(buf.get(), size, format.c_str(), args...);
+			std::snprintf(buf.get(), size, format.c_str(), to_cstr(args)...);
 			return std::string(buf.get(), buf.get() + size - 1);
 	}
 
